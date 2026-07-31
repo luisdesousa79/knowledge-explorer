0f08c6dbc35a9f02467072f41bb5e255fc4f7043
luisdesousa79 <luisfran19@gmail.com>
2026-07-31 17:28:37 +0100

    Create first FastAPI endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile
+
 
 app = FastAPI(title="Knowledge Explorer")
 
@@ -7,4 +9,31 @@ app = FastAPI(title="Knowledge Explorer")
 def root():
     return {
         "message": "Knowledge Explorer is alive!"
+    }
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+@app.post("/users")
+def create_user(user: UserCreate):
+
+    print(user)
+
+    return {
+        "message": f"Olá {user.name}!"
+    }
+
+@app.post("/documents")
+async def upload_document(file: UploadFile):
+
+    contents = await file.read()
+
+    print(type(contents))
+
+    print(len(contents))
+
+    return {
+        "filename": file.filename,
+        "size": len(contents)
     }
